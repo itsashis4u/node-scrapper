e14ac7d15b7bc6572452c9dfb2b78902689225da
Ashish <itsashis4u@gmail.com>
2015-05-26 15:04:46 +0530

    Added IMDb scrape

--- server.js
+++ server.js
@@ -6,12 +6,47 @@ var PORT = process.env.PORT | 8880;
 var app = express();
 
 app.get('/scrape', function(req, res){
-res.send("Hello World");
-console.log("Scraping...")
+	res.send("Hello World");
+	console.log("Scraping...");
+
+	url = 'http://www.imdb.com/title/tt1229340/';
+	request(url, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+
+			var title, release, rating;
+			var json = {title: "", release: "", rating:""};
+
+			$('.header').filter(function(){
+				var data = $(this);
+				title = data.children().first().text();
+				
+				release = data.children().last().children().text();
+
+				json.title = title;
+				json.release = release;
+				
+			});
+
+			$('.star-box-details').filter(function(){
+				
+				rating = $(this).children().first().children().text();
+			
+				json.rating = rating;
+			});
+
+			// console.log(json);
+			fs.writeFile('scrape-output.json', JSON.stringify(json, null, 4), function(err){
+				if(err)
+					console.log("Cannot write to file, scrape-output.json");
+				console.log("File successfully written");
+			});
+		}
+	});
 });
 app.get('/', function(req, res){
 res.send("Root");
-})
+});
 app.listen(PORT);
 
 console.log("Listening at http://localhost:"+PORT);
